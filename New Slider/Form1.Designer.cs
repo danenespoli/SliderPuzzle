a0741503bc3e811c2e3a46334f2b667121319b19
danenespoli <danenespoli@gmail.com>
2015-02-08 01:14:23 -0500

    Initial commit

---
+++ New Slider/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace New_Slider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScramble = new System.Windows.Forms.Button();
+            this.cbDimX = new System.Windows.Forms.ComboBox();
+            this.lblMovesText = new System.Windows.Forms.Label();
+            this.lblMoves = new System.Windows.Forms.Label();
+            this.cbDimY = new System.Windows.Forms.ComboBox();
+            this.lblDimXText = new System.Windows.Forms.Label();
+            this.lblDimYText = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.pbSolved = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSolved)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnScramble
+            // 
+            this.btnScramble.Location = new System.Drawing.Point(613, 26);
+            this.btnScramble.Name = "btnScramble";
+            this.btnScramble.Size = new System.Drawing.Size(128, 46);
+            this.btnScramble.TabIndex = 0;
+            this.btnScramble.Text = "Scramble!";
+            this.btnScramble.UseVisualStyleBackColor = true;
+            this.btnScramble.Click += new System.EventHandler(this.btnScramble_Click_1);
+            // 
+            // cbDimX
+            // 
+            this.cbDimX.FormattingEnabled = true;
+            this.cbDimX.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5\t",
+            "6",
+            "7",
+            "8\t",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30"});
+            this.cbDimX.Location = new System.Drawing.Point(613, 98);
+            this.cbDimX.Name = "cbDimX";
+            this.cbDimX.Size = new System.Drawing.Size(62, 21);
+            this.cbDimX.TabIndex = 1;
+            // 
+            // lblMovesText
+            // 
+            this.lblMovesText.AutoSize = true;
+            this.lblMovesText.Location = new System.Drawing.Point(613, 201);
+            this.lblMovesText.Name = "lblMovesText";
+            this.lblMovesText.Size = new System.Drawing.Size(42, 13);
+            this.lblMovesText.TabIndex = 2;
+            this.lblMovesText.Text = "Moves:";
+            // 
+            // lblMoves
+            // 
+            this.lblMoves.AutoSize = true;
+            this.lblMoves.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMoves.Location = new System.Drawing.Point(661, 201);
+            this.lblMoves.Name = "lblMoves";
+            this.lblMoves.Size = new System.Drawing.Size(14, 13);
+            this.lblMoves.TabIndex = 3;
+            this.lblMoves.Text = "0";
+            // 
+            // cbDimY
+            // 
+            this.cbDimY.FormattingEnabled = true;
+            this.cbDimY.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5\t",
+            "6",
+            "7",
+            "8\t",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30"});
+            this.cbDimY.Location = new System.Drawing.Point(679, 98);
+            this.cbDimY.Name = "cbDimY";
+            this.cbDimY.Size = new System.Drawing.Size(62, 21);
+            this.cbDimY.TabIndex = 4;
+            // 
+            // lblDimXText
+            // 
+            this.lblDimXText.AutoSize = true;
+            this.lblDimXText.Location = new System.Drawing.Point(613, 82);
+            this.lblDimXText.Name = "lblDimXText";
+            this.lblDimXText.Size = new System.Drawing.Size(12, 13);
+            this.lblDimXText.TabIndex = 5;
+            this.lblDimXText.Text = "x";
+            // 
+            // lblDimYText
+            // 
+            this.lblDimYText.AutoSize = true;
+            this.lblDimYText.Location = new System.Drawing.Point(682, 82);
+            this.lblDimYText.Name = "lblDimYText";
+            this.lblDimYText.Size = new System.Drawing.Size(12, 13);
+            this.lblDimYText.TabIndex = 6;
+            this.lblDimYText.Text = "y";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(613, 125);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(128, 25);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // pbSolved
+            // 
+            this.pbSolved.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbSolved.Location = new System.Drawing.Point(613, 280);
+            this.pbSolved.Name = "pbSolved";
+            this.pbSolved.Size = new System.Drawing.Size(128, 128);
+            this.pbSolved.TabIndex = 8;
+            this.pbSolved.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 567);
+            this.Controls.Add(this.pbSolved);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.lblDimYText);
+            this.Controls.Add(this.lblDimXText);
+            this.Controls.Add(this.cbDimY);
+            this.Controls.Add(this.lblMoves);
+            this.Controls.Add(this.lblMovesText);
+            this.Controls.Add(this.cbDimX);
+            this.Controls.Add(this.btnScramble);
+            this.Name = "Form1";
+            this.Text = "Slide Puzzle!";
+            this.SizeChanged += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSolved)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScramble;
+        private System.Windows.Forms.ComboBox cbDimX;
+        private System.Windows.Forms.Label lblMovesText;
+        private System.Windows.Forms.Label lblMoves;
+        private System.Windows.Forms.ComboBox cbDimY;
+        private System.Windows.Forms.Label lblDimXText;
+        private System.Windows.Forms.Label lblDimYText;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.PictureBox pbSolved;
+    }
+}
+
---
+++ New Slider/Form1.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace New_Slider
+{
+    public partial class Form1 : Form
+    {
+        bool scrambled = false;
+        bool scrambling = false;
+        int moves = 0;
+        int dimX = 3;
+        int dimY = 3;
+
+        int last;
+        int buffer;
+        int btnDimX;
+        int btnDimY;
+        int padding;     
+        Button[] buttons;
+        Point[] ogPos;
+        Point[] locations;
+
+        Image img;
+        Size btnSize;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            AltGenerate(1);
+            pbSolved.BackgroundImage = img;
+            cbDimX.Text = "3";
+            cbDimY.Text = "3";
+        }
+
+        //SPLIT IMAGE
+        public Image splitImage(Image img, Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            Bitmap bmpCrop = bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+            return (Image)(bmpCrop);
+        }
+
+        //ALTERNATE GENERATION
+        public void AltGenerate(int type)
+        {       
+            last = (dimX * dimY) - 1;
+            buttons = new Button[dimX * dimY];
+            ogPos = new Point[last];    //1 less than 'buttons', excludes empty button
+            buffer = 1;
+            btnDimX = Convert.ToInt32(((this.Height * 0.9) / dimX) - buffer);   //button dimensions proportional to size of form, leave a 10% padding on top and bottom
+            btnDimY = Convert.ToInt32(((this.Height * 0.9) / dimY) - buffer);
+            padding = 2; //(this.Height - Convert.ToInt32(this.Height * 0.9)) / 2;
+            int lineCount = 1;
+            int count = 1;
+            Point buttonPos = new Point(padding, padding);  //initial button position is just the padding on x/y
+            btnSize = new Size(btnDimX,btnDimY);
+
+            for (int n = 0; n <= last; n++)
+            {
+                    buttons[n] = new Button();   //generation
+                    buttons[n].Width = btnDimX;      //for
+                    buttons[n].Height = btnDimY;         //all
+                    buttons[n].Location = buttonPos;         //buttons
+                    buttons[n].FlatStyle = FlatStyle.Popup;
+
+                if (type == 2)
+                {
+                    buttons[n].Location = locations[n];        
+                }
+
+                if (n == last)     //final button ("empty button")
+                {
+                    buttons[n].Name = "empty";
+                    buttons[n].Visible = false;
+                    buttons[n].Enabled = false;
+                }
+                else
+                {                                       //other buttons
+                    buttons[n].Name = "btn" + (n + 1);
+                    //buttons[n].Text = (n + 1).ToString();
+                    buttons[n].Click += new EventHandler(this.buttons_Click);   //button click event for movement
+                    ogPos[n] = buttonPos;   //to detect if solved         
+                }
+
+                //background image
+                img = new Bitmap(Properties.Resources.pic, new Size(this.Height, this.Height));
+                Rectangle cropRect = new Rectangle(buttonPos, (Size)btnSize);
+                buttons[n].BackgroundImage = splitImage(img, cropRect);
+
+
+                if (lineCount == dimX)      //not at end of y
+                {
+                    buttonPos = new Point(padding, padding + (count * (btnDimY + buffer)));
+                    lineCount = 1;
+                    count += 1;
+                }
+                else
+                {
+                    buttonPos.X += btnDimX + buffer;
+                    lineCount += 1;
+                }
+                this.Controls.Add(buttons[n]);
+
+                
+            }
+        }
+
+        //GENERATATE BUTTONS (SAME X/Y DIM)
+        //public void Generate()
+        //{           
+        //    int last = (dimX * dimX) - 1;
+        //    buttons = new Button[dimX * dimX];
+        //    ogPos = new Point[last];    //1 less than 'buttons', excludes empty button
+        //    int buffer = 30 / dimX;
+        //    int padding = Convert.ToInt32((this.Height * 0.043));
+        //    int lineCount = 1;
+        //    int count = 1;
+
+        //    Point buttonPos = new Point(padding, padding);  //initial button position is just the padding on x/y
+        //    int btnDim = Convert.ToInt32(((this.Height * 0.9) / dimX) - buffer);   //button dimensions proportional to size of form, leave a 10% padding on top and bottom
+
+
+        //    for (int n = 0; n <= last; n++)
+        //    {
+        //        if (n == last)     //final button ("empty button")
+        //        {
+        //            buttons[n] = new Button();
+        //            buttons[n].Name = "empty";
+        //            buttons[n].Width = btnDim;
+        //            buttons[n].Height = btnDim;
+        //            buttons[n].Location = buttonPos;
+        //            buttons[n].Text = "empty";
+        //            buttons[n].Visible = false;
+        //        }
+        //        else
+        //        {                                       //generate all buttons
+        //            buttons[n] = new Button();
+        //            buttons[n].Name = "btn" + (n + 1);
+        //            buttons[n].Width = btnDim;
+        //            buttons[n].Height = btnDim;
+        //            buttons[n].Text = (n + 1).ToString();
+        //            buttons[n].Location = buttonPos;
+        //            buttons[n].Click += new EventHandler(this.buttons_Click);
+        //            ogPos[n] = buttonPos;
+        //        }
+
+        //        if (lineCount == dimX)      //not at end of y
+        //        {
+        //            buttonPos = new Point(padding, padding + (count * (btnDim + buffer)));
+        //            lineCount = 1;
+        //            count += 1;
+        //        }
+        //        else
+        //        {
+        //            buttonPos.X += btnDim + buffer;
+        //            lineCount += 1;
+        //        }
+        //        this.Controls.Add(buttons[n]);
+        //    }
+        //}
+
+        //CHECK IF SOLVED
+        public bool check()
+        {
+            int last = (dimX * dimY) - 1;
+
+                for (int n = 0; n < last; n++)
+                {
+                    if (ogPos[n] != buttons[n].Location)    //if the original position of any button doesn't
+                    {                                       //equal its current position, return false
+                        return false;
+                    }
+                }
+                return true;            //else return true, it is solved
+        }
+
+        //MOVE CONTROLS ON FORM RESIZE
+        public void AdjustControls()    
+        {
+            Point scrambleLoc = new Point(this.Width - 187, btnScramble.Location.Y);    //Repositions all controls   
+            btnScramble.Location = scrambleLoc;
+            Point UpdateLoc = new Point(this.Width - 187, btnUpdate.Location.Y);
+            btnUpdate.Location = UpdateLoc;
+
+            Point dimXLoc = new Point(this.Width - 187, cbDimX.Location.Y);
+            cbDimX.Location = dimXLoc;
+            Point dimXTextLoc = new Point(this.Width - 187, lblDimXText.Location.Y);
+            lblDimXText.Location = dimXTextLoc;
+            Point dimYLoc = new Point(this.Width - 121, cbDimY.Location.Y);
+            cbDimY.Location = dimYLoc;
+            Point dimYTextLoc = new Point(this.Width - 121, lblDimYText.Location.Y);
+            lblDimYText.Location = dimYTextLoc;
+            
+            Point MovesLoc = new Point(this.Width - 139, lblMoves.Location.Y);
+            lblMoves.Location = MovesLoc;
+            Point MovesTextLoc = new Point(this.Width - 187, lblMovesText.Location.Y);
+            lblMovesText.Location = MovesTextLoc;
+            Point solvedLoc = new Point(this.Width - 187, pbSolved.Location.Y);
+            pbSolved.Location = solvedLoc;
+        }
+
+        //REMOVE ALL BUTTONS
+        public void ClearGrid()     
+        {
+            foreach (var v in buttons)
+            {
+                this.Controls.Remove(v);
+            }
+        }
+
+        //SCRAMBLE
+        public void Scramble()     
+        {
+            int last = (dimX * dimY) - 1;     //declare random/last item
+            var rnd = new Random();
+
+
+            for (int n = 0; n < 40 * ((dimX + dimY) / 2); n++)
+            {
+                int r = rnd.Next(0, last);  //from first to last button, generate random    
+                buttons[r].PerformClick();  //click the random button (move it)
+
+                System.Threading.Thread.Sleep(2);   //pause    
+
+                if (check() == true)
+                {
+                    n = 0;
+                }
+            }
+
+
+            scrambled = true;  
+            moves = 0;     //reset moves
+            lblMoves.Text = (moves.ToString());
+        }   
+        
+        //SLIDE PIECES
+        public void buttons_Click(object sender, EventArgs e)  
+        {   
+            Button btn = sender as Button;
+            Point tempLoc;
+            int last = (dimX * dimY) - 1;
+            int buffer = 5;
+            int btnDimX = Convert.ToInt32(((this.Height * 0.9) / dimX) - buffer);   
+            int btnDimY = Convert.ToInt32(((this.Height * 0.9) / dimY) - buffer);
+
+            int xDist = btnDimX + buffer;
+            int yDist = btnDimY + buffer;
+
+
+            //Move single button
+            if ((btn.Location.X == buttons[last].Location.X - xDist || btn.Location.X == buttons[last].Location.X + xDist) && (btn.Location.Y == buttons[last].Location.Y) ||
+                (btn.Location.Y == buttons[last].Location.Y - yDist || btn.Location.Y == buttons[last].Location.Y + yDist) && (btn.Location.X == buttons[last].Location.X))
+            {
+                moves += 1;     //increment move counter
+                lblMoves.Text = (moves.ToString());
+
+                tempLoc = buttons[last].Location;        //swap location of btn and empty space (buttons[last])
+                buttons[last].Location = btn.Location;
+                btn.Location = tempLoc;
+            }
+            //Move multiple buttons 
+            else if (btn.Location.X == buttons[last].Location.X)    //same X, diff Y (vertical)
+            {
+                moves += 1;     //increment move counter
+                lblMoves.Text = (moves.ToString());
+
+                if (btn.Location.Y < buttons[last].Location.Y)  
+                {
+                    multiSlideYDown(btn, yDist);                  //above empty button
+                }
+                else
+                {
+                    multiSlideYUp(btn, yDist * -1);               //below                    
+                }
+            }
+            else if (btn.Location.Y == buttons[last].Location.Y)    //same Y, diff X (horizontal)
+            {
+                moves += 1;     //increment move counter
+                lblMoves.Text = (moves.ToString());
+
+                if (btn.Location.X < buttons[last].Location.X)  
+                {
+                    multiSlideXRight(btn, xDist);                 //left of empty button                    
+                }
+                else
+                {
+                    multiSlideXLeft(btn, xDist * -1);             //right of
+                }
+            }
+            
+            
+
+
+            //if SOLVED (scrambled, buttons in correct place, not in the middle of scrambling)
+            if (scrambled == true && check() == true && scrambling == false)
+            {
+                buttons[last].Visible = true;    
+                MessageBox.Show("You won in " + lblMoves.Text + " moves!");
+                scrambled = false;  //reset 'scrambled'   
+                foreach (var v in buttons)
+                {
+                    v.Enabled = false;
+                }
+            }          
+        }
+
+        //MULTI SLIDE PIECES     
+        public void multiSlideYDown(Button btn, int dist)
+        {
+            Point beforePos = btn.Location;         
+            for (int n = 0; n < last; n++)   //because n is 1, it will actually only go to just before the last button
+            {
+                if (buttons[n].Location.X == buttons[last].Location.X && (buttons[n].Location.Y < buttons[last].Location.Y && buttons[n].Location.Y >= btn.Location.Y))
+                {
+                    buttons[n].Location = new Point(buttons[n].Location.X, buttons[n].Location.Y + dist);  //move down
+                }
+            }
+            buttons[last].Location = beforePos;  //move empty button to button clicked
+        }   //DOWN
+        public void multiSlideYUp(Button btn, int dist)
+        {
+            Point beforePos = btn.Location;
+            for (int n = 0; n < last; n++)   //because n is 1, it will actually only go to just before the last button
+            {
+                if (buttons[n].Location.X == buttons[last].Location.X && (buttons[n].Location.Y > buttons[last].Location.Y && buttons[n].Location.Y <= btn.Location.Y))
+                {
+                    buttons[n].Location = new Point(buttons[n].Location.X, buttons[n].Location.Y + dist);  //move up (dist * -1 as arg)
+                }
+            }
+            buttons[last].Location = beforePos;  //move empty button to button clicked
+        }   //UP
+        public void multiSlideXRight(Button btn, int dist)
+        {
+            Point beforePos = btn.Location;
+            for (int n = 0; n < last; n++)   //because n is 1, it will actually only go to just before the last button
+            {
+                if (buttons[n].Location.Y == buttons[last].Location.Y && (buttons[n].Location.X < buttons[last].Location.X && buttons[n].Location.X >= btn.Location.X))
+                {
+                    buttons[n].Location = new Point(buttons[n].Location.X + dist, buttons[n].Location.Y);  //move right
+                }
+            }
+            buttons[last].Location = beforePos;  //move empty button to button clicked
+        }   //RIGHT
+        public void multiSlideXLeft(Button btn, int dist)
+        {
+            Point beforePos = btn.Location;
+            for (int n = 0; n < last; n++)   //because n is 1, it will actually only go to just before the last button
+            {
+                if (buttons[n].Location.Y == buttons[last].Location.Y && (buttons[n].Location.X > buttons[last].Location.X && buttons[n].Location.X <= btn.Location.X))
+                {
+                    buttons[n].Location = new Point(buttons[n].Location.X + dist, buttons[n].Location.Y) ;  //move right
+                }
+            }
+            buttons[last].Location = beforePos;  //move empty button to button clicked
+        }   //LEFT
+        
+      
+
+
+
+
+
+
+
+            //Image[] images = new Image[last];
+            //Graphics g = Graphics.FromImage(img);
+            //Brush redBrush = new SolidBrush(Color.Red);
+            //Pen pen = new Pen(redBrush, 3);
+            //MessageBox.Show(img.Width + " " + img.Height);
+            //for (int i = 0; i < img.Width; i = (img.Width / 3) + i)
+            //{
+            //    for (int y = 0; y < last; y = (img.Height / 3) + y)
+            //    {
+            //        Rectangle r = new Rectangle(i, y, img.Width / dimX, img.Height / dimY);
+            //        g.DrawRectangle(pen, r);
+            //        if (i > 0 && y > 0)
+            //        {
+            //            if (i + r.Width < b.Width && y + r.Height < b.Height)
+            //            {
+                            
+            //            }
+            //        }
+            //    }
+            //}
+            //g.Dispose();
+
+        private void btnScramble_Click_1(object sender, EventArgs e)    //Scramble button click
+        {
+            buttons[last].Visible = false;
+
+            scrambling = true;  //can't give 'solved' prompt
+            do
+            {
+                Scramble();     //scramble once
+            }
+            while (check() == true);    //keep scrambling until it's not in solved state
+            scrambling = false;
+        }
+
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            AdjustControls();
+            locations = new Point[last];
+
+            for (int i = 0; i <= last; i++)
+            {
+                locations[i] = buttons[i].Location;
+            }
+            ClearGrid();
+            AltGenerate(2); //regen so that positions are kept; without unscrambling 
+            scrambled = false;
+                      
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            foreach (var v in buttons)
+            {
+                v.Enabled = true;
+            }
+            
+            dimX = Convert.ToInt32(cbDimX.SelectedItem);
+            dimY = Convert.ToInt32(cbDimY.SelectedItem);
+            ClearGrid();
+            AltGenerate(1);
+            moves = 0;
+            lblMoves.Text = moves.ToString();
+            scrambled = false;
+        }
+
+        }
+}
+
